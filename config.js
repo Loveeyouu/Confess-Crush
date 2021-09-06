fc2182f27901a6102cf74ed15dc367214947085b
Loveeyouu <90153145+Loveeyouu@users.noreply.github.com>
2021-09-06 14:20:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     reply: 'Không làm gì cũng tự hết giận hihi <333333',
     mess: 'Anh biết em có bề ngoài mạnh mẽ nhưng thực chất có một trái tim khá yếu đuối',
     messDesc: 'Hết dịch anh sẽ bù đắp cho những tổn thương của em nhaaaaaaaaaaaaaaa 😘😘',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <33',
     messDesc: 'Trong thời gian dịch vừa rồi, đã có rất nhiều vấn đề xảy ra. Anh biết em là một người dễ cáu nhưng lại hết giận rất nhanh. Dù vậy, anh cũng không muốn em cáu dù chỉ là một chút. Nhưng anh đã vô ý làm em buồn và phải suy nghĩ nhiều chuyện và anh thật lòng xin lỗi vì điều đấy. Anh là người khá cứng đầu, thiếu kiên nhẫn, hay tùy hứng và đôi lúc vô tâm với em và anh đang cố gắng để sửa những sai lầm đó. Nói nhiều không bằng hành động nhiều nên trong thời gian tới anh sẽ chứng minh cho em thấy những điều đó là sự thật.',
     messLink: 'https://www.facebook.com/ngao1312' 
 }
